18506d9175b06e1c24d1fd4e75e39733596b552d
JosefStonon <josef.sartori@outlook.com>
2024-02-07 08:52:19 -0300

    up

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,3 +8,7 @@
  export const config = {
    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+//psql ""
+
+//postgres://default:gCXezMD0AKU7@ep-old-queen-80987926-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb
